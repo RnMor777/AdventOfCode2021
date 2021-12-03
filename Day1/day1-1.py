4ee97b7bd1ab875c6c7f654c783b9b5933094dfd
RnMor777 <ryan.morganti@trojans.dsu.edu>
2021-12-03 14:35:23 -0600

    Removed old file names

--- Day1/day1-1.py
+++
@@ -1,3 +0,0 @@
-f = open("day1.txt", "r")
-arr = list(map(int, f.readlines()))
-print(sum([1 if arr[i+1]>arr[i] else 0 for i in range(len(arr)-1)]))
--- Day1/day1-2.py
+++
@@ -1,3 +0,0 @@
-f = open("day1.txt", "r")
-arr = list(map(int, f.readlines()))
-print(sum([1 if sum(arr[i+1:i+4])>sum(arr[i:i+3]) else 0 for i in range(len(arr))]))
